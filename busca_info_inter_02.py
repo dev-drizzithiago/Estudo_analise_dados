922aa9d02ff0ae3f62b2db4216af04d9e738d5f3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-03-28 11:07:42 -0300

    atualização

--- busca_info_inter_02.py
+++ busca_info_inter_02.py
@@ -1,9 +1,18 @@
 import requests
+import lxml
 from bs4 import BeautifulSoup
 
+busca_2 = []
+
 site_html = requests.get('https://www.climatempo.com.br/').content
-soup = BeautifulSoup(site_html, 'html.parser')
+soup = BeautifulSoup(site_html, 'lxml')
 
 busca_1 = soup.find_all('a')
-for links in busca_1:
-    print(links.get('href'))
+for valor in busca_1:
+    busca_2.append(valor)
+
+for links in enumerate(busca_2):
+    if links == soup.get('href'):
+        print(links)
+
+print('ERRO')
